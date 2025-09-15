4b799fa6ca2952453d65fd0e415c57a50d66cde4
Pravachan <pravachan47@gmail.com>
2025-09-15 19:07:58 +0000

    this one

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -42,28 +42,34 @@ export function UserIcon(props: SVGProps<SVGSVGElement>) {
 
 export function VidyaanLogo(props: SVGProps<SVGSVGElement>) {
   return (
-    <div className="flex items-center justify-center gap-2" >
+    <div className="flex items-center justify-center" >
         <svg
             {...props}
-            viewBox="0 0 200 100"
+            viewBox="0 0 200 120"
             xmlns="http://www.w3.org/2000/svg"
         >
             <defs>
-                <linearGradient id="logo-grad" x1="0%" y1="0%" x2="100%" y2="100%">
-                    <stop offset="0%" style={{stopColor: 'rgb(74, 222, 128)', stopOpacity: 1}} />
-                    <stop offset="100%" style={{stopColor: 'rgb(59, 130, 246)', stopOpacity: 1}} />
+                <linearGradient id="logo-grad" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" style={{stopColor: '#4FD1C5', stopOpacity: 1}} />
+                    <stop offset="100%" style={{stopColor: '#F6E05E', stopOpacity: 1}} />
                 </linearGradient>
             </defs>
             
             {/* Icon */}
-            <g transform="translate(10, 0) scale(0.4)">
-                <path fill="url(#logo-grad)" d="M80 10L10 40L80 70L150 40L80 10Z" />
-                <path fill="#1E3A8A" d="M80 75C110 90 140 75 140 75V115C140 115 110 130 80 115C50 130 20 115 20 115V75C20 75 50 90 80 75Z" />
-                <path fill="#F59E0B" d="M148 41L148 61L152 59V39L148 41Z" />
+            <g transform="translate(55, 0)">
+                {/* Book */}
+                <path fill="#2c5282" d="M69.92,44.32c-3.9-2.27-8.75-3.56-13.84-3.56H56c-0.03,0-0.06,0-0.09,0c-5.12,0.01-9.99,1.3-13.9,3.58 C38.1,46.56,34.56,49.53,32,53.07v30.08c0,0,12-6.68,24-0.1c12,6.58,24,0,24,0V53.05 C77.44,49.52,73.89,46.55,69.92,44.32z"/>
+
+                {/* Cap */}
+                <g>
+                    <path fill="url(#logo-grad)" d="M56,12.11l-36,15v4.67l36,15l36-15v-4.67L56,12.11z"/>
+                    <path fill="#F6E05E" d="M89,32.78v4h2v-4.67L89,32.78z"/>
+                    <rect x="52" y="32.11" fill="#2c5282" width="8" height="12"/>
+                </g>
             </g>
 
             {/* Text */}
-            <text x="80" y="55" fontFamily="Inter, sans-serif" fontSize="30" fill="#1E3A8A" textAnchor="start" fontWeight="bold">
+            <text x="100" y="110" fontFamily="Inter, sans-serif" fontSize="24" fill="#2c5282" textAnchor="middle" fontWeight="bold">
                 Vidyaan
             </text>
         </svg>
