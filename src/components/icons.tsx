26fc60dc07316f8b2b45fe466194c9b3d2cd2081
Pravachan <pravachan47@gmail.com>
2025-09-16 16:03:14 +0000

    make it more impressive and profecenal (_for element <VidyaanLogo>_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -21,28 +21,36 @@ export function UserIcon(props: SVGProps<SVGSVGElement>) {
 export function VidyaanLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-        width="150"
-        height="100"
-        viewBox="0 0 100 80"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
+      width="150"
+      height="100"
+      viewBox="0 0 100 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g transform="translate(25, 0)">
+        {/* Graduation Cap */}
+        <path d="M10 20L25 12.5L40 20L25 27.5L10 20Z" fill="hsl(var(--primary))" />
+        {/* Book */}
+        <path
+          d="M10 22.5 C 10 27.5, 20 32.5, 25 32.5 C 30 32.5, 40 27.5, 40 22.5 L 40 35 C 40 40, 30 45, 25 45 C 20 45, 10 40, 10 35 L 10 22.5 Z"
+          fill="hsl(var(--primary))"
+          opacity="0.8"
+        />
+        {/* Tassel */}
+        <path d="M40 20 L 45 20 L 45 27.5" stroke="hsl(var(--primary))" strokeWidth="1.5" strokeLinecap="round" />
+      </g>
+      <text
+        x="50"
+        y="65"
+        fontFamily="Inter, Arial, sans-serif"
+        fontSize="20"
+        fontWeight="800"
+        fill="hsl(var(--foreground))"
+        textAnchor="middle"
       >
-        <g transform="translate(25, 0)">
-            <path d="M10 20L20 15L30 20L20 25L10 20Z" fill="hsl(var(--primary))" />
-            <path d="M20 25L30 20L40 25L30 30L20 25Z" fill="hsl(var(--primary))" opacity="0.7" />
-        </g>
-        <text
-          x="50"
-          y="65"
-          fontFamily="Inter, Arial, sans-serif"
-          fontSize="20"
-          fontWeight="800"
-          fill="hsl(var(--foreground))"
-          textAnchor="middle"
-        >
-          vidyaan
-        </text>
-      </svg>
+        vidyaan
+      </text>
+    </svg>
   );
 }
